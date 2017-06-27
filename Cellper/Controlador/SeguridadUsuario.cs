89f15836d42c2cf68a6a034ae861c5c8babea7aa
Carlos Ceballos <carceb@hotmail.com>
2017-06-27 16:05:45 -0400

    Configuracion de la interfaz de usuario

---
+++ Cellper/Controlador/SeguridadUsuario.cs
@@ -0,0 +1,52 @@
+﻿using Database.Classes;
+using Seguridad.Clases;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Seguridad
+{
+    public partial class SeguridadUsuario
+    {
+        public static int InsertarUsuario(CSeguridad objetoSeguridad)
+        {
+            SqlParameter[] dbParams = new SqlParameter[]
+            {
+                    DBHelper.MakeParam("SeguridadUsuarioDatosID", SqlDbType.Int, 0, objetoSeguridad.SeguridadUsuarioDatosID),
+                    DBHelper.MakeParam("@LoginUsuario", SqlDbType.VarChar, 0, objetoSeguridad.LoginUsuario),
+                    DBHelper.MakeParam("@ClaveUsuario", SqlDbType.VarChar, 0, objetoSeguridad.ClaveUsuario),
+                    DBHelper.MakeParam("@NombreCompleto", SqlDbType.VarChar, 0, objetoSeguridad.NombreCompleto),
+                    DBHelper.MakeParam("@DescripcionUsuario", SqlDbType.VarChar, 0, objetoSeguridad.DescripcionUsuario),
+                    DBHelper.MakeParam("@SeguridadGrupoID", SqlDbType.Int, 0, objetoSeguridad.SeguridadGrupoID),
+                    DBHelper.MakeParam("@UsuarioTecnico", SqlDbType.Bit, 0, objetoSeguridad.UsuarioTecnico),
+                    DBHelper.MakeParam("@EstatusUsuario", SqlDbType.VarChar, 0,objetoSeguridad.EstatusUsuario)
+            };
+            if (objetoSeguridad.SeguridadUsuarioDatosID == 0)
+            {
+                return Convert.ToInt32(DBHelper.ExecuteScalar("[usp_SeguridadUsuario_Insertar]", dbParams));
+            }
+            else
+            {
+                return Convert.ToInt32(DBHelper.ExecuteScalar("[usp_SeguridadUsuario_Actualizar]", dbParams));
+            }
+        }
+        public static DataSet ObtenerLogin(string loginDeUsuario)
+        {
+            SqlParameter[] dbParams = new SqlParameter[]
+                {
+                    DBHelper.MakeParam("@LoginUsuario", SqlDbType.VarChar, 0, loginDeUsuario),
+                };
+
+            return DBHelper.ExecuteDataSet("usp_SeguridadUsuario_ObtenerLogin", dbParams);
+        }
+        public static SqlDataReader ObtenerEmpresas(int usuarioID)
+        {
+            SqlParameter[] dbParams = new SqlParameter[]
+                {
+                    DBHelper.MakeParam("@SeguridadUsuarioDatosID", SqlDbType.Int, 0, usuarioID),
+                };
+
+            return DBHelper.ExecuteDataReader("usp_Login_ObtenerEmpresas", dbParams);
+        }
+    }
+}
---
+++ Cellper/Vista/UCNavigation.ascx.cs
@@ -0,0 +1,97 @@
+﻿using Admin;
+using Seguridad.Clases;
+using System;
+using System.Web.UI;
+
+namespace Teach
+{
+    public partial class UCNavigation : System.Web.UI.UserControl
+    {
+        private void Page_Load(object sender, EventArgs e)
+        {
+
+            bool Logged = paginaBase.LoggedIn(Session);
+
+            if (this.lnkChangePassword.Enabled)
+            {
+                this.lnkChangePassword.CssClass = "izq";
+            }
+            else
+            {
+                this.lnkChangePassword.CssClass = "izqDesactivado";
+            }
+
+
+            Page.ClientScript.RegisterStartupScript(Page.GetType(), "tttt", "ocultarEnlaces(" + Logged.ToString().ToLower() + ");", true);
+            this.divLogin.Visible = !Logged;
+            if (Logged)
+            {
+                int codigoDeTecnico = Convert.ToInt32(this.Session["UserId"].ToString());
+                CSeguridad objetoSeguridad = new CSeguridad();
+                objetoSeguridad.SeguridadUsuarioDatosID = codigoDeTecnico;
+                lnkHome.CssClass = "izq";
+
+                if (objetoSeguridad.EsUsuarioAdministrador() == true)
+                {
+                    this.lblVentas.CssClass = "izq";
+                    this.lnkRecepcionEquipos.CssClass = "izq";
+                    this.lnkColaDeServicios.CssClass = "izq";
+                    this.lnkColaDeServicios.CssClass = "izq";
+                    this.lblConsultas.CssClass = "izq";
+                    this.lnkClientesAtendidos.CssClass = "izq";
+                    this.lnkSeguridad.CssClass = "izq";
+
+                    this.lblOpciones.CssClass = "izq";
+                    this.lnkInventario.CssClass = "izq";
+                    this.lnkClientes.CssClass = "izq";
+                }
+                else
+                {
+                     this.lnkSeguridad.Visible = false;
+                    //MENU ATENCION VISITAS
+                    if (objetoSeguridad.EsAccesoPermitido(1) == true)
+                    {
+                        this.lblVentas.CssClass = "izq";
+                        this.lnkRecepcionEquipos.CssClass = "izq";
+                        this.lnkColaDeServicios.CssClass = "izq";
+                        
+                    }
+                    else
+                    {
+                        this.lblVentas.Visible = false;
+                        this.lnkRecepcionEquipos.Visible = false;
+                        this.lnkColaDeServicios.Visible = false;
+                    }
+
+
+
+                    //MENU CONSULTAS
+                    if (objetoSeguridad.EsAccesoPermitido(2) == true)
+                    {
+                        this.lblConsultas.CssClass = "izq";
+                        this.lnkClientesAtendidos.CssClass = "izq";
+                    }
+                    else
+                    {
+                        this.lblConsultas.Visible = false;
+                        this.lnkClientesAtendidos.Visible = false;
+                    }
+
+                }
+
+            }
+        }
+
+
+
+        protected override void OnInit(EventArgs e)
+        {
+            this.InitializeComponent();
+            base.OnInit(e);
+        }
+        private void InitializeComponent()
+        {
+            base.Load += new EventHandler(this.Page_Load);
+        }
+    }
+}
